235ca8d4c80ba22cfc1315f4d55580745b517036
Alinario Junior <alinariojunior@gmail.com>
2019-02-22 11:29:54 -0300

    Metodos de Lista

--- DAO/class/usuario.php
+++ DAO/class/usuario.php
@@ -68,6 +68,47 @@ class Usuario {
 
     }
 
+    public static function getList(){
+
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+
+    } 
+
+    public static function search($login){
+
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_usuarios where deslogin LIKE :SEARCH ORDER BY deslogin", array(
+            ":SEARCH"=>"%".$login."%"
+        ));
+    }
+
+    public function login($usuario, $senha){
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_usuarios
+      WHERE deslogin = :USUARIO AND dessenha = :SENHA", array(
+            ":USUARIO" => $usuario,
+            ":SENHA" => $senha
+        ));
+
+        if (count($results) > 0) {
+
+            $row = $results[0];
+
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+        }else{
+            throw new Exception("Login e/ou senha inválidos");
+        }
+
+    }
+
     public function __toString() {
 
         return json_encode(array(
--- DAO/index.php
+++ DAO/index.php
@@ -2,11 +2,39 @@
 
 require_once("config.php");
 
+//carrega um usuário usando o login e a senha
+
+$usuario = new Usuario();
+
+$usuario->login("user","12345");
+
+echo $usuario;
+
+
+//Carrega uma lista buscando pelo login
+/*
+$search = Usuario::search("jo");
+
+echo json_encode($search);
+*/
+
+//Carrega uma lista de usuários
+/*
+$lista = Usuario::getList();
+
+echo json_encode($lista);
+*/
+
+
+
+//Carrega um usuário
+/*
 $root = new Usuario();
 
 $root->loadById(5);
 
 echo $root;
+*/
 
 /*
 $sql = new Sql();
